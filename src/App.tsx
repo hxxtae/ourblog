712ccfe58a14eef7879755dd4c8430832b4a86a3
hxxtae <fkdlxmfkdl@gmail.com>
2024-02-26 20:04:21 +0900

    :books: [Docs]: Create component of Loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import { useState } from 'react';
-import { getAuth } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { app } from 'firebaseApp';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { app } from 'firebaseApp';
 import Router from 'components/Router';
+import Loader from 'components/Loader';
 
 function App() {
   const auth = getAuth(app);
   // firebase Auth가 인증되었으면 true로 변경해주는 로직 추가
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
+  // NOTE: Loading State (최초 마운트 되었을 때, 새로고침 되었을 때)
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+      setInit(true);
+    });    
+  }, [auth]);
 
   return (
     <>
       <ToastContainer />
-      <Router isAuthenticated={isAuthenticated} />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </>
   );
 }
